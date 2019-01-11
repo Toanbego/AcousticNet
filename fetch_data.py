e76e37b513e23a5b41af876be47dc411846de454
LAPTOP-AFNS1ELH\toanbego <torstein.gombos@gmail.com>
2019-01-11 11:56:57 +0100

    First commite

---
+++ fetch_data.py
@@ -0,0 +1,23 @@
+"""
+Author - Torstein Gombos
+Date - 08.01.2019
+
+This script reads CSV files and uses JSON to to navigate the data that exists on YouTube.
+"""
+
+
+def read_csv():
+    """
+
+    :return:
+    """
+
+
+def main():
+    """
+
+    :return:
+    """
+
+
+main()
